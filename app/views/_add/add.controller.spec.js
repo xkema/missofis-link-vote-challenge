f63c6b21f022db45523716b770dca8175b181d39
Kemal Yılmaz <kemalxyz@gmail.com>
2016-07-04 11:58:17 +0300

    Update add controller test specs

--- app/views/_add/add.controller.spec.js
+++ app/views/_add/add.controller.spec.js
@@ -22,11 +22,15 @@ describe( 'UNIT ::  Controller Test : AddCtrl', function() {
 
 	describe( 'AddCtrl :: Controller Behaviour', function() {
 
-		var __LinkVoteChallengeService;
+		var __LinkVoteChallengeService,
+			$timeout,
+			$rootScope;
 
 		beforeEach( function() {
-			angular.mock.inject( function( LinkVoteChallengeService ) {
+			angular.mock.inject( function( LinkVoteChallengeService, _$timeout_, _$rootScope_ ) {
 				__LinkVoteChallengeService = LinkVoteChallengeService;
+				$timeout = _$timeout_;
+				$rootScope = _$rootScope_;
 			} );
 		} );
 
@@ -34,7 +38,22 @@ describe( 'UNIT ::  Controller Test : AddCtrl', function() {
 			__LinkVoteChallengeService.setAppData( { items: [], userCheated: false }, true ); // reset app data to set item count to "0"			
 			__controller.addLink();
 			var _items = __LinkVoteChallengeService.getAppData().items;
-			expect( _items.length ).toEqual( _items.length );
+			expect( _items.length ).toEqual( 1 );
+		} );
+
+		it( 'should set "disableAdd" flag true before timeout, false after', function() {
+			expect( __controller.disableAdd ).toBe( false ); // initial value
+			__controller.addLink();
+			expect( __controller.disableAdd ).toBe( true ); // addLink called
+			$timeout.flush();
+			expect( __controller.disableAdd ).toBe( false ); // re-stored by $timeout
+		} );
+
+		it( 'should fire "mso.showToaster" event to show link added toaster', function() {
+			spyOn( $rootScope, '$emit' );
+			__controller.addLink();
+			$timeout.flush();
+			expect( $rootScope.$emit ).toHaveBeenCalled();
 		} );
 
 	} );
